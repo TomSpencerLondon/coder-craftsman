ecf86320ba303dfe9ca3b2909fdc5b0db3305d41
Tom Spencer <tom@homeflow.co.uk>
2019-12-08 22:28:37 +0000

    Add description for index page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,20 @@ import Layout from "../components/layout"
 
 const IndexPage = () => (
   <Layout>
+    <div>
+      <h1></h1>
+      <p>
+        Formerly a teacher, I am now a web developer. I have completed a Makers Academy 12 week coding course and have now worked as a web developer on Front End and Back End at Homeflow. Homeflow is an award winning SaaS business building estate agency websites and developing software to help estate agents to perform better online.
+        My work at Homeflow has included:
+        <ul>
+          <li>Helping to turn an online booking system for valuations into a viable product.</li>
+          <li>Building a test presenter for Capybara testing across our websites to ensure that front end site errors are quickly reported.</li>
+          <li>Build and refactor tools for API interaction with Reapit and Rightmove using Nokogiri and SOAP.</li>
+          <li>Devops deploys using Docker</li>
+        </ul>
+        Throughout my work at Homeflow I have been careful to follow best practices for development and have been praised for my strong communication skills, diligence and team work.
+      </p>
+    </div>
   </Layout>
 )
 
